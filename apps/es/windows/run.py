c0435d5b7e0a67420ae30e98dac4d238015cb5f1
root <root@396.vm.devbox.dev>
2021-03-31 10:19:06 +0800

    fix es connect error

--- apps/es/windows/run.py
+++ apps/es/windows/run.py
@@ -13,10 +13,14 @@ async def scan(host, port, index, body, account, password):
 
     logger.info("[ES] APP执行参数为：{host} {port} {index} {body}", host=host, port=port, index=index, body=body)
 
-    if account != None and str(account) != "" and password != None and str(password) != "":
-        es = Elasticsearch(hosts=host, port=port, http_auth=(account, password))
-    else:
-        es = Elasticsearch(hosts=host, port=port)
+    try:
+        if account != None and str(account) != "" and password != None and str(password) != "":
+            es = Elasticsearch(hosts=host, port=port, http_auth=(account, password))
+        else:
+            es = Elasticsearch(hosts=host, port=port)
 
-    result = es.search(index=index, body=body)
+
+        result = es.search(index=index, body=body)
+    except Exception as e:
+        return {"status": 2, "result": "ES连接失败:"+str(e)}
     return {"status": 0, "result": str(result)}
